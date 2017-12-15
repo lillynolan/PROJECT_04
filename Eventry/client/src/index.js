0ad8b5cc3b6ddc4fe92741d0623d3012a0b803f2
Lilly Nolan <lillynolan@gmail.com>
2017-12-15 13:30:28 -0500

    imported fontawesome

--- Eventry/client/src/index.js
+++ Eventry/client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+//referenced for importing fontawesome https://stackoverflow.com/questions/23116591/how-to-include-a-font-awesome-icon-in-reacts-render
+
