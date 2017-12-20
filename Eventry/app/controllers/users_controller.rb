f15046d0ad54d4761806a32e2c4863436fa76df1
Lilly Nolan <lillynolan@gmail.com>
2017-12-19 19:29:34 -0500

    took out show to fix post

--- Eventry/app/controllers/users_controller.rb
+++ Eventry/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApiController
   before_action :require_login, except: [:create]
 
+  def index
+   render json: { user: User.all}
+  end
+
   def create
     user = User.create!(user_params)
     render json: { token: user.auth_token }
@@ -9,14 +13,14 @@ class UsersController < ApiController
   def profile
     user = User.find_by_auth_token!(request.headers[:token])
     user_events = Event.where(user_id: user.id)
-    render json: { user: { username: user.username, firstname: user.firstname, lastname: user.lastname },
-    events: user_events,
-     }
+    render json: { user: { username: user.username, firstname: user.firstname, lastname: user.lastname }, events: user_events }
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :password, :firstname, :lastname)
   end
-
 end
+
+
+# referenced react-reails-monsters inclass lecture
